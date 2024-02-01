e5cc5b77ca73f37abc4d1786929aa9a01e4218b7
Carlos Perez <carlos.perez@charliegperez.com>
2024-01-31 20:03:38 -0600

    Feautre: CSS adjustment in sections

--- components/MyData.jsx
+++ components/MyData.jsx
@@ -1,13 +1,16 @@
+import DataImage from './DataImage';
 import style from '../styles/Mydata.module.css';
 
 export default function MyData() {
+  const urlImages =
+    'https://myimages.carlosgperez.com/portfolio/projects/work.webp';
   return (
     <section id="myData" className={`${style.myData}`}>
       <div className={style.myData_header}>
         <h1>Carlos Gerardo Pérez Horta</h1>
         <h2>Software Developer especialista en Frontend</h2>
       </div>
-      <div>
+      <div className={`${style.myDescription}`}>
         <p>
           +6 años de experiencia en el desarrollo Frontend, algunas de las
           tecnologías que manejo son <b>JavaScript</b>, <b>ReactJS</b>, HTML,
@@ -21,6 +24,14 @@ export default function MyData() {
           Soy autodidacta, busco adaptarme rápidamente a los cambios y dispuesto
           a aprender nuevas tecnologías.
         </p>
+      </div>
+      <div>
+        <DataImage
+          src={urlImages}
+          alt="Imagen de trabajo"
+          widthImage={300}
+          heightImage={227}
+        />
       </div>
     </section>
   );
